c1a3a97e049ce65c9182bb796ca5f376bad597cc
Casper Nilsson <caspernilsson13@gmail.com>
2024-06-10 16:29:04 +0200

    Fixed Array Output

--- src/main/java/se/lexicon/StringExercise.java
+++ src/main/java/se/lexicon/StringExercise.java
@@ -36,9 +36,11 @@ public class StringExercise {
         }
         System.out.println("-------------------------");
 
-        String[] arr2 = {"Carl, Susie, Fredrick, Bob, Erik "};
-        for (String u : arr2) {
-            System.out.print(u + " ");
+        String[] names = { "Carl", "Susie", "Fredrick", "Bob", "Erik"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
         }
     }
-}
